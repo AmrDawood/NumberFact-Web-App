8ea24750f57dbeb3ec8dfe4144eada14d70fe298
AmrDawood-codeWebTech <amrdawood91@gmail.com>
2020-09-01 00:23:40 +0200

    year and math
    now you can choose type of number you want to now fact about

--- js/scriptAjax.js
+++ js/scriptAjax.js
@@ -1,10 +1,30 @@
 const inputNumber = document.getElementById("inputNumber");
 const outputFact =document.querySelector("div p");
-inputNumber.addEventListener("input",getnumberFact);
+const btnYear=document.querySelector('#btngroup button:nth-child(3)');
+const btnTrivia=document.querySelector('#btngroup button:nth-child(2)');
+const btnMath=document.querySelector('#btngroup button:nth-child(1)');
+const btnGroup =document.querySelectorAll('#btngroup button');
+
+let targetNumberType;
+let inputNumberValue;
+
+btnGroup.forEach(btn => {
+    btn.addEventListener('click',showInput);
+ });
+
+
+function showInput(e){
+    targetNumberType =e.target.value;
+    inputNumber.style.cssText="display:block !important;";
+    inputNumber.value="";
+    inputNumber.placeholder = `please enter your ${e.target.textContent} number`;
+    inputNumber.addEventListener("input",getnumberFact);
+}
+
 function getnumberFact(){
     let httpReq =new XMLHttpRequest();
-    let inputNumberValue = inputNumber.value
-    httpReq.open('GET' , `https://cors-anywhere.herokuapp.com/http://numbersapi.com/${inputNumberValue}`);
+    inputNumberValue = inputNumber.value ;
+    httpReq.open('GET' , `https://cors-anywhere.herokuapp.com/http://numbersapi.com/${inputNumberValue}/${targetNumberType}`);
     httpReq.send();
     httpReq.addEventListener("readystatechange",()=>{
         if(httpReq.status == 200 && httpReq.readyState==4 && inputNumberValue!=""){
@@ -12,5 +32,5 @@ function getnumberFact(){
         }else if(inputNumberValue ==""){
             outputFact.textContent = "";
         }
-    })
+    });
 }

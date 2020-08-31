6b80bd80c3fa3dfe020352add811a27f73625533
AmrDawood-codeWebTech <amrdawood91@gmail.com>
2020-08-31 22:19:16 +0200

    append https
    error mixed contend :
    github using HTTPS , the browser will refuse to load any resources over HTTP.and our number api useing http, so changing the API URL to have HTTPS instead of HTTP

--- js/scriptAjax.js
+++ js/scriptAjax.js
@@ -4,7 +4,7 @@ inputNumber.addEventListener("input",getnumberFact);
 function getnumberFact(){
     let httpReq =new XMLHttpRequest();
     let inputNumberValue = inputNumber.value
-    httpReq.open('GET' , `http://numbersapi.com/${inputNumberValue}`);
+    httpReq.open('GET' , `https://cors-anywhere.herokuapp.com/http://numbersapi.com/${inputNumberValue}`);
     httpReq.send();
     httpReq.addEventListener("readystatechange",()=>{
         if(httpReq.status == 200 && httpReq.readyState==4 && inputNumberValue!=""){
